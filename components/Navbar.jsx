f46b62611607dfe6070005b914fe6e99187fedd3
bharathkumar369 <bharathpathapati@gmail.com>
2024-04-04 20:55:52 +0530

    navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,57 @@
 import React from "react";
-import { View,Image } from "react-native";
-import dictionary from "../assets/dictionary"
+import { View,Image,StyleSheet } from "react-native";
+// import dictionary from "../assets/dictionary"
 
 
 const Navbar = () => {
     return(
-        <main>
-            <Image source={dictionary}/>
-        </main>
+        <View style={styles.container}>
+            <View>
+                <Image
+                    source={require("./images/dictionary.png")}
+                    style={styles.image}
+                />
+            </View>
+            <View style={styles.align}>
+                <View>
+                    <Image
+                        source={require("./images/toggle-off-white.png")}
+                        style={styles.sideImage}
+                    />
+                </View>
+                <View>
+                    <Image
+                            source={require("./images/moon.png")}
+                            style = {styles.sideImage}
+                        />
+                </View>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image:{
+        height:60,
+        width:60,
+    },
+    sideImage:{
+        height:40,
+        width:40
+    },
+    container:{
+        paddingHorizontal:20,
+
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-between" ,
+        alignItems:"center"
+    },
+    align:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"space-between" ,
+        gap:20
+    }
+})
 export default Navbar;
--- components/Search.jsx
+++ components/Search.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { View,Text,StyleSheet,Image, TextInput } from "react-native"
-import book from "../assets/book.svg"
+
 import UseFetch from "../hooks/useFetch";
 
 const Search = () => {
     return(
         <View>
-            <View>
-                <Image 
-                    style={{width:50,height:50}}
-                    source={book}
-                />
-            </View>
             <View style={styles.input}>
                 <TextInput
                     placeholder="Search word"
                     
+                />
+            </View>
+            <View>
+                <Image
+                    source={require("./images/search-color.png")}
+                    style={styles.sideImage}
                 />
             </View>
         </View>
